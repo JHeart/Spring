189e0f8dff3dd0fc8f7bee0a1871cfbb02603c7f
JHeart <medusa3290@naver.com>
2018-03-29 15:40:39 +0900

    firstCommit

---
+++ workspace/eStore/src/main/java/kr/ac/hansung/cse/controller/ProductController.java
@@ -0,0 +1,30 @@
+package kr.ac.hansung.cse.controller;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import kr.ac.hansung.cse.model.Product;
+import kr.ac.hansung.cse.service.ProductService;
+
+
+@Controller
+public class ProductController {//이 클래스에 Bin을 자동으로 관리
+
+	//Controller -> service ->dao
+	@Autowired
+	private ProductService productServices;
+	
+	@RequestMapping("/products")// url을 보내게 되면 아래의 method가 수행
+	public String getProducts(Model model) {
+		
+		List<Product> products = productServices.getProducts();
+		model.addAttribute("products", products); //위의 products를 ("products", products)의 하단부에 
+		
+		return "products";// viewㅇ's logical name
+		
+	}
+}
